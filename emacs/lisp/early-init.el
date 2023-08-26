a371c4845c1afe031a272a616bf053cf78295b79
Hayato Ito <hayatoito@gmail.com>
2023-08-26 23:34:08 +0900

    Test workflow

---
+++ emacs/lisp/early-init.el
@@ -0,0 +1,13 @@
+;;; early-init.el --- -*- lexical-binding: t; -*-
+
+;;; Commetary:
+
+;;; Code:
+
+;;; Basic
+
+(setq package-enable-at-startup nil)
+
+(setq load-prefer-newer t)
+
+;;; early-init.el ends here
---
+++ emacs/lisp/init.el
@@ -0,0 +1,11 @@
+;;; init.el --- -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+;;; Path
+
+(message "init.el ends here")
+
+;;; init.el ends here
