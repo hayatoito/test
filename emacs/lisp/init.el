70c6f2b1011c5c6c85a80a65da876ef5553080f0
Hayato Ito <hayatoito@gmail.com>
2023-08-27 12:01:39 +0900

    fixup! Emacs package debug

--- emacs/lisp/init.el
+++ emacs/lisp/init.el
@@ -8,8 +8,8 @@
 
 (require 'my-path (locate-user-emacs-file "lisp/my-path"))
 
-(messae "package-user-dir: %s" package-user-dir)
-(messae "package-directory-list: %s" package-directory-list)
+(message "package-user-dir: %s" package-user-dir)
+(message "package-directory-list: %s" package-directory-list)
 
 (message "(directory-files package-user-dir): %s" (directory-files package-user-dir))
 
